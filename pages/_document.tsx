32731f7e510e00ceacab23ec0aa018a6bf2e9e59
longxingren <973506608@qq.com>
2020-09-27 18:19:09 +0800

    Fix: font load issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -11,33 +11,32 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css?family=Inter:400,600,700&display=swap" rel="stylesheet" />
-          <Head>
-            <link href="/favicon/favicon.ico" rel="shortcut icon" />
-            <link href="/favicon/site.webmanifest" rel="manifest" />
-            <link
-              rel="dns-prefetch"
-              href="https://fonts.googleapis.com/"
-              crossOrigin=""
-            />
-            <link
-              href="/favicon/apple-touch-icon.png"
-              rel="apple-touch-icon"
-              sizes="180x180"
-            />
-            <link
-              href="/favicon/favicon-32x32.png"
-              rel="icon"
-              sizes="32x32"
-              type="image/png"
-            />
-            <link
-              href="/favicon/favicon-16x16.png"
-              rel="icon"
-              sizes="16x16"
-              type="image/png"
-            />
-          </Head>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" media="all"/>
+          <link href="/favicon/favicon.ico" rel="shortcut icon" />
+          <link href="/favicon/site.webmanifest" rel="manifest" />
+          <link
+            href="/favicon/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicon/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicon/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
         </Head>
         <body>
           <Main />
