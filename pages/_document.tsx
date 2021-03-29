5b413b45f16daf13bee805d9df2238c08b774ed8
longxingren <973506608@qq.com>
2021-03-29 17:18:52 +0800

    feat: pWA enabled
    added workbox-webpack-plugin;
    
    BREAKING CHANGE: PWA enabled

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,31 +9,58 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="zh">
         <Head>
           <script
             async
             type="text/javascript"
             /* language=javascript */
-            dangerouslySetInnerHTML={{ __html: `
-              try {
-                var query = window.matchMedia("(prefers-color-scheme: dark)");
-                var preference = window.localStorage.getItem("ke1vin-blog-theme");
-                window.__LOCAL_THEME__ = preference;
-              
-                if (preference) {
-                  if ((preference === "SYSTEM" && query.matches) || preference === "DARK") {
-                    document.documentElement.classList.add("dark-theme");
+            dangerouslySetInnerHTML={{
+              __html: `
+                try {
+                  const query = window.matchMedia("(prefers-color-scheme: dark)");
+                  const preference = window.localStorage.getItem("ke1vin-blog-theme");
+                  window.__LOCAL_THEME__ = preference;
+                
+                  if (preference) {
+                    if ((preference === "SYSTEM" && query.matches) || preference === "DARK") {
+                      document.documentElement.classList.add("dark-theme");
+                    }
                   }
+                } catch (e) {
+                  console.error(e);
                 }
-              } catch (e) {
-                console.error(e);
-              }
-            ` }}
+              `,
+            }}
           />
-          <link href="https://fonts.googleapis.com/css?family=Inter:400,600,700&display=swap" rel="stylesheet" />
+          <script
+            async
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                 if ('serviceWorker' in navigator) {
+                   window.addEventListener('load', () => {
+                     navigator.serviceWorker.register('/sw.js').then(registration => {
+                       console.log('SW registered: ', registration);
+                     }).catch(registrationError => {
+                       console.log('SW registration failed: ', registrationError);
+                     });
+                   });
+                 }
+              `,
+            }}
+          />
+          <meta name='application-name' content="Ke1vin's Blog" />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content="Ke1vin's Blog" />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#000000' />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link crossOrigin="anonymous"  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
           <link href="/favicon/favicon.ico" rel="shortcut icon" />
-          <link href="/favicon/site.webmanifest" rel="manifest" />
+          <link href="/manifest.json" rel="manifest" />
           <link
             rel="dns-prefetch"
             href="https://fonts.googleapis.com/"
---
+++ service-worker.js
@@ -0,0 +1,115 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { setCacheNameDetails } from 'workbox-core';
+import {
+  CacheFirst,
+  StaleWhileRevalidate,
+  NetworkFirst,
+} from 'workbox-strategies';
+import {
+  registerRoute,
+  NavigationRoute,
+} from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import * as navigationPreload from 'workbox-navigation-preload';
+
+const CACHE_NAME = 'offline-html';
+const FALLBACK_HTML_URL = '/offline.html';
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.add(FALLBACK_HTML_URL))
+  );
+
+  self.skipWaiting();
+});
+
+setCacheNameDetails({
+  prefix: 'ke1vin-blog',
+  precache: 'precache',
+  runtime: 'runtime',
+  googleAnalytics: 'google-analytics-name',
+});
+
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+navigationPreload.enable();
+
+const networkFirst = new NetworkFirst({
+  cacheName: 'pages',
+  plugins: [
+    new ExpirationPlugin({
+      maxAgeSeconds: 24 * 60 * 60, // 1 Day
+    }),
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+});
+
+const navigationHandler = async (params) => {
+  try {
+    // Attempt a network request.
+    return await networkFirst.handle(params);
+  } catch (error) {
+    // If it fails, return the cached HTML.
+    console.log(await caches.match('/offline.html'));
+    return caches.match(FALLBACK_HTML_URL, {
+      cacheName: CACHE_NAME,
+    });
+  }
+};
+
+registerRoute(
+  new NavigationRoute(navigationHandler)
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.gstatic.com',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  new CacheFirst({
+    cacheName: 'static-resources',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
